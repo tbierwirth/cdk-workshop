664d71cde40c3ac1885b1ee815feaec249322004
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 12:06:00 -0700

    Add table viewer

--- lib/hitcounter.d.ts
+++ lib/hitcounter.d.ts
@@ -1,9 +1,11 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 export interface HitCounterProps {
     downstream: lambda.IFunction;
 }
 export declare class HitCounter extends Construct {
     readonly handler: lambda.Function;
+    readonly table: dynamodb.Table;
     constructor(scope: Construct, id: string, props: HitCounterProps);
 }
