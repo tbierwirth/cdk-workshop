2d02e6bd9ecf23fb53ee3879aee5c4f8185c3874
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 15:29:09 -0700

    Switch to codebuildstep from shellstep

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, CodeBuildStep } from 'aws-cdk-lib/pipelines';
 
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,10 +8,11 @@ export class WorkshopPipelineStack extends cdk.Stack {
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('tbierwirth/cdk-workshop', 'main', {
-          authentication: cdk.SecretValue.secretsManager('github-token')
-        }),
+      synth: new CodeBuildStep('SynthStep', {
+        input: CodePipelineSource.gitHub('tbierwirth/cdk-workshop', 'main'),
+        installCommands: [
+          'npm install -g aws-cdk'
+        ],
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     })
