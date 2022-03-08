ad38066a87752220e5d5cfa9c9bbd0755b89e056
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 15:53:15 -0700

    Update to expose hitcounter and tableviewer endpoints

--- lib/cdk-workshop-stack.d.ts
+++ lib/cdk-workshop-stack.d.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 export declare class CdkWorkshopStack extends cdk.Stack {
+    readonly hcViewerUrl: cdk.CfnOutput;
+    readonly hcEndpoint: cdk.CfnOutput;
     constructor(scope: Construct, id: string, props?: cdk.StackProps);
 }
