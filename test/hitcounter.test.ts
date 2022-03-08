bf0fbdf8916cd340837b7bb19d070fa10087ec94
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 12:54:43 -0700

    Add encryption to dynamodb

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -49,3 +49,22 @@ test('Lambda Has Environment Variables', () => {
     }
   );
 });
+
+test('DynamoDB Table Created With Encryption', () => {
+  const stack = new cdk.Stack();
+
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  })
+
+  const template = Template.fromStack(stack)
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    SSESpecification: {
+      SSEEnabled: true
+    }
+  })
+})
