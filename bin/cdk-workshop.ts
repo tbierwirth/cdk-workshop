7a2fa9261f0fc517ba096f34d194cb7c9cca5f0b
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 14:12:12 -0700

    Remove old workshop stack

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 import { WorkshopPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
 new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack', {
   env: {
     account: process.env.CDK_AWS_ACCOUNT,
