420bf5bdff9cb7bb90e8c27bbdb292b5cc1fec4f
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 14:09:23 -0700

    Add github code pipeline

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import { WorkshopPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
 new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack', {
+  env: {
+    account: process.env.CDK_AWS_ACCOUNT,
+    region: process.env.REGION
+  }
+});
+
+app.synth();
