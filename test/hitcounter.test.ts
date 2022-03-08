69b6343de45859ea17d0ad9a2172837be8ac5826
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-08 12:48:12 -0700

    Add test for environment variables

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -2,6 +2,7 @@ import { Template, Capture } from "aws-cdk-lib/assertions";
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { HitCounter } from "../lib/hitcounter";
+import { table } from "console";
 
 test('DynamoDB Table Created', () => {
   const stack = new cdk.Stack();
@@ -17,3 +18,34 @@ test('DynamoDB Table Created', () => {
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::DynamoDB::Table", 1);
 })
+
+test('Lambda Has Environment Variables', () => {
+  const stack = new cdk.Stack();
+
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
+
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual(
+    {
+      Variables: {
+        DOWNSTREAM_FUNCTION_NAME: {
+          Ref: "TestFunction22AD90FC",
+        },
+        HITS_TABLE_NAME: {
+          Ref: "MyTestConstructHits24A357F0",
+        },
+      },
+    }
+  );
+});
